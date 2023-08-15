348bac041b507d2087db6d28b1799762710cb0e6
x86skwizer <yassineamrire@protonmail.com>
2023-08-14 18:21:40 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, n in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(n), end=" ")
+            else:
+                print("{:d}".format(n), end="")
+        print("$")
