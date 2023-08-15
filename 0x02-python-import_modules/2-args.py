3527c380a7b92737a0a58098e113776b456246f2
x86skwizer <yassineamrire@protonmail.com>
2023-08-15 15:16:21 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    print("{} arguments.".format(len(sys.argv) - 1))
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    elif len(sys.argv) > 2:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments.".format(len(sys.argv) - 1))
     i = 1
     while i < len(sys.argv):
         print("{}: {}".format(i, sys.argv[i]))
