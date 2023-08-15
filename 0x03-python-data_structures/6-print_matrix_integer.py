f60608cdf965614ceb1c9fd404baaae134a0ec42
x86skwizer <yassineamrire@protonmail.com>
2023-08-14 18:31:47 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(n), end=" ")
             else:
                 print("{:d}".format(n), end="")
-        print("$")
+        print()
