8861a833272c5eae6c23752ff7eabc8d6a1afcd5
x86skwizer <yassineamrire@protonmail.com>
2023-08-16 14:52:54 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = [x for x in my_list]
     i = 0
     while i < len(new_list):
-        if  new_list[i] == search:
+        if new_list[i] == search:
             new_list[i] = replace
         i += 1
     return new_list
