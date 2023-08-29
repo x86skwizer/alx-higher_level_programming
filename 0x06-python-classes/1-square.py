391728ca706157ceeeb6633aad363b24211f8005
x86skwizer <yassineamrire@protonmail.com>
2023-08-29 10:53:14 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A class Square that defines a square"""
+    
+    def __init__(self, _Square__size):
+        self._Square__size = _Square__size
