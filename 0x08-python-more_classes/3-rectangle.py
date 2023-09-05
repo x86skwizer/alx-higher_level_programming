5d02e741f17aa502c80f87b1ef8c41191b2e4578
x86skwizer <yassineamrire@protonmail.com>
2023-09-04 18:38:31 -0700

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines class Rectangle that represent a rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """The width property."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """The width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """The height property."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """The height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate rectangle's area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculate rectangle's perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        matrix = ""
+        for j in range(self.height - 1):
+            matrix += "#" * self.width + "\n"
+        matrix += "#" * self.width
+        return matrix
