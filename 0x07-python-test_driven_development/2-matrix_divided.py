91d6f0f3ce423213c0962b5b5c28fa90a88f1a3f
x86skwizer <yassineamrire@protonmail.com>
2023-09-09 19:04:14 -0700

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,17 +15,20 @@ def matrix_divided(matrix, div):
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
     sizer = len(matrix[0])
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats")
         if len(row) != sizer:
             raise TypeError("Each row of the matrix must have the same size")
         nwr = []
         for ele in row:
             if not isinstance(ele, int) and not isinstance(ele, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats")
             nwr.append(round(ele / div, 2))
         reslt.append(nwr)
     return reslt
