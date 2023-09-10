2b483278cd1da6a73dcea58e3b64740aaf0f270c
x86skwizer <yassineamrire@protonmail.com>
2023-09-09 18:00:55 -0700

    6-hash_table_delete.c

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,20 +13,17 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists)"
-                "of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     sizer = len(matrix[0])
     for row in matrix:
-        if not isinstance(matrix, list):
-            raise TypeError("matrix must be a matrix (list of lists)"
-                    "of integers/floats")
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         if len(row) != sizer:
             raise TypeError("Each row of the matrix must have the same size")
         nwr = []
         for ele in row:
             if not isinstance(ele, int) and not isinstance(ele, float):
-                raise TypeError("matrix must be a matrix (list of lists)"
-                        "of integers/floats")
-            nwr.append(round(ele / div, 2))
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            nwr.append(round(float(ele), 2) / round(float(div), 2))
         reslt.append(nwr)
     return reslt
