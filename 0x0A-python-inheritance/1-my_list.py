a800ec02c291be7b23a2973e49826023630cb253
x86skwizer <yassineamrire@protonmail.com>
2023-09-12 01:33:55 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class MyList that inherits from list"""
+
+class MyList(list):
+    """Class MyList"""
+
+    def print_sorted(self):
+        """Prints the list"""
+        print(sorted(self))
