24d6e5c8ce0504e80b9a0e4612f2993c2d4a962e
x86skwizer <yassineamrire@protonmail.com>
2023-09-16 05:16:58 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ Otherwise False
 
 def inherits_from(obj, a_class):
     """
-    Returns True if object is instance of class inherited 
+    Returns True if object is instance of class inherited
     Otherwise False
     """
     return True if type(obj) != a_class else False
