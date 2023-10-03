2f68a557bc87d93079a6b95bd65abe785e3bf741
x86Skwizer <yassineamrire@protonmail.com>
2023-10-03 03:07:43 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Function that reads text file (UTF8) and prints it to stdout"""
-
-
-def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end='')
