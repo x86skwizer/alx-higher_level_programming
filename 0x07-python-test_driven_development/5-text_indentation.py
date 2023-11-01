205552090d3b3cc11d8504b02752f0cf17a8cfe0
x86skwizer <yassineamrire@protonmail.com>
2023-10-31 19:50:05 -0700

    6-max_integer.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a function that prints a text with 2 new lines
+Prototype: def text_indentation(text):
+text must be a string
+"""
+
+
+def text_indentation(text):
+    """
+    Text indentation.
+    """
+    
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Test function.
+    """
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-1, 2, 0, 5]), 5)
+
+    def test_duplicate_numbers(self):
+        self.assertEqual(max_integer([1, 2, 2, 1]), 2)
+
+if __name__ == '__main__':
+    """
+    Main function.
+    """
+    unittest.main()
