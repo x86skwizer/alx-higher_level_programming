7954d122fc167c80dac00050355f2daa0694b0e7
x86skwizer <yassineamrire@protonmail.com>
2023-11-06 18:01:56 -0800

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Improve Geometry"""
 
+
 class BaseGeometry:
     """class BaseGeometry"""
-    
+
     def area(self):
         """instance method"""
         raise Exception("area() is not implemented")
