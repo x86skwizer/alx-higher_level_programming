c5a85d40a7bf6bcd73933ba9b45f1a4ddf4f244d
x86skwizer <yassineamrire@protonmail.com>
2023-11-16 21:13:27 -0800

    8-update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,6 +85,19 @@ class Rectangle(Base):
                 print("#", end="")
             print("")
 
+    def update(self, *args):
+        """Update the class"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+
     def __str__(self):
         """Overriding __str__ method"""
         spos = f"{self.x}/{self.y} - {self.width}/{self.height}"
