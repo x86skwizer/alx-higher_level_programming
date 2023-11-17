78afd9293ab3c9bbe40802b24c8955bcbbe8f0c9
x86skwizer <yassineamrire@protonmail.com>
2023-11-17 01:58:55 -0800

    base_to_json_string

--- 0x0C-python-almost_a_circle/14-main.py
+++ 0x0C-python-almost_a_circle/14-main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" 14-main """
+""" Check """
+import inspect
 from models.base import Base
-from models.rectangle import Rectangle
 
-if __name__ == "__main__":
+to_json_string_fct = Base.__dict__.get("to_json_string")
+if to_json_string_fct is None:
+    print("Base doesn't have method to_json_string")
+    exit(1)
 
-    r1 = Rectangle(10, 7, 2, 8)
-    dictionary = r1.to_dictionary()
-    json_dictionary = Base.to_json_string([dictionary])
-    print(dictionary)
-    print(type(dictionary))
-    print(json_dictionary)
-    print(type(json_dictionary))
+if type(to_json_string_fct) is not staticmethod:
+    print("to_json_string is not a static method")
+    exit(1)
+
+print("OK", end="")
