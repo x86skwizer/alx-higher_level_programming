212829cc9e28de265fb75cb093894b0d0d3a1a0a
x86skwizer <yassineamrire@protonmail.com>
2023-11-17 00:43:33 -0800

    12-square_update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,6 +21,22 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Update Square attributes"""
+        if len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+                self.height = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """Representative string of Square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
