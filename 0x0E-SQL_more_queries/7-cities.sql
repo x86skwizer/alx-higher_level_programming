aee083993a8bd00ab79101074568910466c59961
x86skwizer <yassineamrire@protonmail.com>
2023-12-13 15:50:55 -0800

    7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,3 @@
+-- Creates the database hbtn_0d_usa and the table cities on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
