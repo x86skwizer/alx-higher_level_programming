0d606232142aca823511f4edca935edd414ba65f
x86skwizer <yassineamrire@protonmail.com>
2023-12-13 15:35:53 -0800

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creates the database hbtn_0d_usa and the table states on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT AUTO_INCREMENT UNIQUE NOT NULL, name VARCHAR(256) NOT NULL);
