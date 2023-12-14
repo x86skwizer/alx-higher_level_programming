7b19e7ed8420690d52f73fc80d1e771382f68d8d
x86skwizer <yassineamrire@protonmail.com>
2023-12-13 16:46:20 -0800

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,4 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_shows_genres.genre_id FROM hbtn_0d_tvshows ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+SELECT tv_shows.title, tv_shows_genres.genre_id FROM hbtn_0d_tvshows 
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;
