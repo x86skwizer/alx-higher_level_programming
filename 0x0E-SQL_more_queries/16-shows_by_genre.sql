235be5f5016c3a10f9442094b356ed7090a883c9
x86skwizer <yassineamrire@protonmail.com>
2023-12-13 17:54:06 -0800

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS genre_name
+SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
