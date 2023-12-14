39bb23906bfbb8f7290671f7b7fe1314c944fb5b
x86skwizer <yassineamrire@protonmail.com>
2023-12-13 17:59:51 -0800

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre_name ASC;
+ORDER BY title ASC, name ASC;
