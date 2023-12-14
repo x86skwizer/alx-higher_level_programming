9bfd2a3d17ba9ecb71072ae28d9110fa84f10aba
x86skwizer <yassineamrire@protonmail.com>
2023-12-13 16:52:47 -0800

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows.tv_shows, hbtn_0d_tvshows.tv_show_genres 
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
