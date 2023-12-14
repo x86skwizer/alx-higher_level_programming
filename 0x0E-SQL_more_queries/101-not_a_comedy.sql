d09f75ff4cc4146c05e1772e089f858990b4cf5d
x86skwizer <yassineamrire@protonmail.com>
2023-12-13 18:25:53 -0800

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,11 @@
 -- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 SELECT title
 FROM tv_shows
-WHERE id NOT IN (SELECT show_id FROM tv_show_genres WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy'))
+WHERE title NOT IN
+(SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
 ORDER BY title ASC;
