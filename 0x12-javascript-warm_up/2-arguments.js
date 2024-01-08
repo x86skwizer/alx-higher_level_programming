b803e084546fc3b37272800e34497a59ccae83bc
x86skwizer <yassineamrire@protonmail.com>
2024-01-08 14:04:00 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const argc = process.argv.length - 2;
-if (argc == 0) {
+if (argc === 0) {
   console.log('No argument');
-}
-else if (argc == 1) {
+} else if (argc === 1) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
 }
