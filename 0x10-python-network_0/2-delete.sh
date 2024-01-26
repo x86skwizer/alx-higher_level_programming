d3faa93aac57bcbe042162d6336f410f1fe3c640
x86skwizer <yassineamrire@protonmail.com>
2024-01-26 03:49:29 +0100

    0x10-python-network_0

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a DELETE request to the URL and display the response body.
+curl -sX DELETE "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """ Test function find_peak """
+    if not list_of_integers:
+        return None
+    return max(list_of_integers)
